753a9d78f2f9af009ee7157cb6bb21a119f2a75a
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-04-12 11:45:49 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let matn = String(prompt("string turdagi ma'lumot kriting"));
+let son  =  Number(prompt("Number turdagi ma'lumot kriting"));
+let rost = Boolean(prompt("boolean turdagi ma'lumot kriting"));
+
+ confirm("Hammama'lumotlar to'g'rimi");
+
+ alert("siznng kiritgan" +matn +sozi  +typeof matn+"typega tenshli" );
+ alert("siznng kiritgan" +son +soni  +typeof matn+"typega tenshli" );   
